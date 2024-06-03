09648fbeb32189157f340b84a28892607ec0ff7e
Maor Attias <maorattias17@gmail.com>
2024-06-03 10:24:20 +0300

    Card and HomeCards components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <HomeCards />
     </div>
   )
 }
